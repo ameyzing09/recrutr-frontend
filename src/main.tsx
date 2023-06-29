2a106486fcc76402684c1f262f9c6cd372c17e57
Amey Kode <ameykode2001@gmail.com>
2023-06-29 20:19:05 +0530

    Added login api with reducer testing remaining

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux/es/exports";
 import "./index.css";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
